a0fafaaaa5295f64c38f8956f44ebb33e72207b7
PROGRAMACION <jgutierrrezg@miumg.edu.gt>
2022-09-18 18:22:40 -0600

    No1

---
+++ PRO2EXAMEN2/adapter_ej/patterns_6_adapter_ejercicio/src/org/formacion/Adapter.java
@@ -0,0 +1,18 @@
+package org.formacion;
+
+public class Adapter implements InternationalMoneyOrganization {
+
+	@Override
+	public void transfer(int cantidad, String cliente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int state(String cliente) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
